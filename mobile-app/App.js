0ce20aef0bc9c00173fb2f3f030e00d095f57af4
Piotr Stefanowicz <stefan8357@gmail.com>
2018-04-20 12:06:24 +0200

    Creating react native app

---
+++ mobile-app/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import MyComponent from './src/components/MyComponent'
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <MyComponent/>
+    );
+  }
+}
